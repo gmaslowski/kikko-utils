6f059bd257099849e9255b5c774e0387c91595d9
gmaslowski <grzegorz.maslowski@kikko.pl>
2013-09-21 16:46:35 +0200

    Fix.

--- kikko-test/src/main/java/pl/kikko/test/unit/AbstractUnitTest.java
+++ kikko-test/src/main/java/pl/kikko/test/unit/AbstractUnitTest.java
@@ -9,7 +9,7 @@ import static org.mockito.MockitoAnnotations.initMocks;
 public class AbstractUnitTest {
 
     @Rule
-    protected ExpectedException expectedException = ExpectedException.none();
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void injectMocks() {
